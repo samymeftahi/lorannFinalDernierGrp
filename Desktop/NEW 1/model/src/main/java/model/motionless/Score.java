abd6b0ac17d996963bcb50f731cf0eaeeeee6ec0
samymeftahi <samymeftahi@gmail.com>
2018-06-05 13:26:14 +0000

    j'ai encore faim

--- Desktop/NEW 1/model/src/main/java/model/motionless/Score.java
+++
@@ -1,7 +0,0 @@
-package model.motionless;
-
-/** Created by Romain Muller */
-
-/** Class used to create the score */
-public class Score extends Empty {
-}
--- Desktop/NEW 1/model/src/main/java/model/motionless/Title.java
+++
@@ -1,7 +0,0 @@
-package model.motionless;
-
-/** Created by Romain Muller */
-
-/** Class used to create the title */
-public class Title extends Empty {
-}
